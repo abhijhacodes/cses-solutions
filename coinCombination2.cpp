9e4a050a5d66e62b8fd80f2c7db48a98ec84ec43
abhijhacodes <abhi.jha.cs@gmail.com>
2021-07-18 10:36:27 +0530

    added coin combinations

---
+++ coinCombination2.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    ll n,x; 
+    cin >> n >> x;
+    vector<ll> coins(n);
+    for(ll i=0; i<n; i++) cin >> coins[i];
+
+    vector<ll> dp(x+1, 0);
+    dp[0] = 1;
+
+    for(ll coin: coins)
+    {
+        for(ll i=1; i<=x; i++)
+        {
+            if(i >= coin)
+                dp[i] = (dp[i] + dp[i-coin])%mod;
+        }
+    }
+    cout << dp[x]%mod;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
